ddf951cd4d8517aa7a832ba90d2b2479f7b5acba
Johannes Denninger <joxz@users.noreply.github.com>
2019-05-22 14:48:11 +0200

    d

--- sf.go
+++ sf.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+
+	"regexp"
 	"strings"
 	"time"
 
@@ -22,8 +25,6 @@ type Sfprefix struct {
 	IPPrefix string `json:"ip_prefix" db:"ip_prefix"`
 }
 
-type Sfregion []string
-
 func scrapesf(url string) (html string) {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
@@ -40,28 +41,58 @@ func scrapesf(url string) (html string) {
 	return
 }
 
-func parsehtml(html string) (sfpref []Sfprefix) {
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+func parsehtml(html *string) (sfpref []Sfprefix) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(*html))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
-		doc.Find("td").Each(func(j int, sel *goquery.Selection) {
-			if sel.Text() == "RIPE" {
-				fmt.Println(sel.Children().Length(), sel.Text(), sel.Next().Text())
+	re := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\/[0-9]{1,2}`)
+
+	var reg string
+
+	doc.Find("td").Each(func(_ int, s *goquery.Selection) {
+		var sp Sfprefix
+
+		val, ok := s.Attr("colspan")
+		if ok && val == "2" {
+			if s.Text() == "Australia (Public Cloud)" {
+				reg = "Australia"
+			} else if s.Text() == "Canada (Public Cloud)" {
+				reg = "Canada"
+			} else {
+				reg = s.Text()
 			}
-		})
+		}
+		match := re.FindString(s.Text())
+		sp.IPPrefix = match
+		sp.Region = reg
+
+		if sp.IPPrefix != "" {
+			sfpref = append(sfpref, sp)
+
+		}
 	})
 
 	return
 }
 
 func main() {
+
 	out := scrapesf("https://help.salesforce.com/articleView?id=000003652&type=1")
 
 	//fmt.Println(out)
-	prefixes := parsehtml(out)
-	fmt.Println(prefixes)
+	prefixes := parsehtml(&out)
 
+	x := Sfnode{
+		Created:  time.Now(),
+		Prefixes: prefixes,
+	}
+
+	outj, err := json.MarshalIndent(x, "", "    ")
+	if err != nil {
+		log.Fatal("JSON marshal error. ", err)
+	}
+
+	fmt.Println(string(outj))
 }
